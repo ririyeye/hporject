d6f8ec3ce090219c1565274e4d6c9af2aa659434
ririyeye <200610237@qq.com>
2018-11-03 14:10:44 +0800

    add udp relay

--- ss/ss-up.sh
+++ ss/ss-up.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 SOCKS_SERVER=149.28.14.216 # SOCKS 服务器的 IP 地址
 PORT=1080
 # Setup the ipset
@@ -11,23 +10,22 @@ done
 # 在nat表中新增一个链，名叫：SHADOWSOCKS
 iptables -t nat -N SHADOWSOCKS
 
-# Allow connection to the server
-iptables -t nat -A SHADOWSOCKS -d $SOCKS_SERVER -j RETURN
-
-# Allow connection to reserved networks
-iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+ipset add chnroute 0.0.0.0/8
+ipset add chnroute 10.0.0.0/8
+ipset add chnroute 127.0.0.0/8
+ipset add chnroute 169.254.0.0/16
+ipset add chnroute 172.16.0.0/12
+ipset add chnroute 192.168.0.0/16
+ipset add chnroute 224.0.0.0/4
+ipset add chnroute 240.0.0.0/4
+ipset add chnroute $SOCKS_SERVER
 
 # Allow connection to chinese IPs
 iptables -t nat -A SHADOWSOCKS -p tcp -m set --match-set chnroute dst -j RETURN
 # 如果你想对 icmp 协议也实现智能分流，可以加上下面这一条
 iptables -t nat -A SHADOWSOCKS -p icmp -m set --match-set chnroute dst -j RETURN
+
+
 
 # Redirect to Shadowsocks
 # 把1081改成你的shadowsocks本地端口
@@ -44,3 +42,12 @@ iptables -t nat -A OUTPUT -p icmp -j SHADOWSOCKS
 iptables -t nat -A PREROUTING -s 192.168/16 -j SHADOWSOCKS
 # 内网流量源NAT
 iptables -t nat -A POSTROUTING -s 192.168/16 -j MASQUERADE
+
+
+ip rule add fwmark 0x01/0x01 table 100
+ip route add local 0.0.0.0/0 dev lo table 100
+iptables -t mangle -N SSUDP
+iptables -t mangle -A SSUDP -p udp -m set --match-set chnroute dst -j RETURN
+iptables -t mangle -A SSUDP -p udp -j TPROXY --on-port $PORT --tproxy-mark 0x01/0x01
+iptables -t mangle -A PREROUTING -j SSUDP
+
